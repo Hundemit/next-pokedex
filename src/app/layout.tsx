dbc5f5114c71c356cd5396be9cb15f4fbf96a487
Jan Hindemit <janhindemit1@gmail.com>
2025-07-29 18:01:54 +0200

    Füge neue Abhängigkeiten für @vercel/analytics und @vercel/speed-insights hinzu und integriere diese in das Layout, um die Analyse- und Leistungsoptimierung der Anwendung zu verbessern.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +33,8 @@ export default function RootLayout({
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
             {children}
           </ThemeProvider>
+          <SpeedInsights />
+          <Analytics />
         </body>
       </html>
     </>
